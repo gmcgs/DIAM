417cf039ddbbc6b5f41ad7e2c690d58d95086276
carlotasousa <Carlota_Sousa@iscte-iul.p>
2023-03-01 22:30:16 +0000

    ex4 done

--- Proj1e3/ex3-4.py
+++ Proj1e3/ex3-4.py
@@ -1,5 +1,6 @@
 import time
 
+
 def alineaA(str1, str2):
     print("alínea a):")
     count = 0
@@ -10,18 +11,20 @@ def alineaA(str1, str2):
     for i in range(len(str1)):
         for j in range(len(str2)):
             count += 1
-            if(str2[j] == str1[i]):
+            if (str2[j] == str1[i]):
                 str2 = str2.replace(str2[j], "X", 1)
     for l in range(len(str2)):
-        if(str2[l] != "X"):
+        if (str2[l] != "X"):
             answer = False
     toc = time.perf_counter()
     print(str1)
     print(str2)
     print(count)
-    print(f"{toc-tic:.8f}")
-    if answer == False: return False
-    else: return True
+    print(f"{toc - tic:.8f}")
+    if answer == False:
+        return False
+    else:
+        return True
 
 
 def alineaB(str1, str2):
@@ -36,25 +39,94 @@ def alineaB(str1, str2):
     print(str1)
     print(str2)
     for i in range(len(str1)):
-        count+=1
-        if(str1[i] != str2[i]): answer = False
-    print(count)
+        count += 1
+        if (str1[i] != str2[i]): answer = False
     toc = time.perf_counter()
-    print(f"{toc-tic:.8f}")
+    print(count)
+    print(f"{toc - tic:.8f}")
     return answer
 
+
+countC = 0
+
+
 def alineaC(str1, str2):
-# variante "força bruta" -> basicamente temos que fazer todas as combinações de caracteres possiveis com a primeira string
-# guardar todas numa lista e no fim comparar com a segunda string e ver se ela está na lista. se tiver return true
-# contar passos e tempo como nas outras alineas
+    print("alínea c):")
+    global countC
+    answer = False
+    tic = time.perf_counter()
+    str1 = str1.replace(" ", "")
+    str2 = str2.replace(" ", "")
+    combinations = findCombinations(str1)
+    for i in range(len(combinations)):
+        countC += 1
+        if str2 == combinations[i]:
+            answer = True
+    toc = time.perf_counter()
+    print(countC)
+    print(f"{toc - tic:.8f}")
+    return answer
+
+
+def findCombinations(str):
+    global countC
+    countC += 1
+    if len(str) == 0:
+        return [""]
+    if len(str) == 1:
+        return [str]
+    else:
+        output = []
+        for i in range(len(str)):
+            current_char = str[i]
+            other_char = str[:i] + str[i + 1:]
+            for x in findCombinations(other_char):
+                output.append(current_char + x)
+        return output
+
 
 def alineaD(str1, str2):
-# temos que contar o numero de vezes que cada caracter aparece em cada string e depois comparar uma com a outra, se for igual da return true
-# contar passos e o tempo como nas outras alineas
+    print("alínea d):")
+    count = 0
+    answer = False
+    tic = time.perf_counter()
+    str1 = str1.replace(" ", "")
+    str2 = str2.replace(" ", "")
+    str1 = sorted(str1)
+    str2 = sorted(str2)
+    dictionary1 = {}
+    dictionary2 = {}
+    for i in str1:
+        count += 1
+        if i in dictionary1:
+            dictionary1[i] += 1
+        else:
+            dictionary1[i] = 1
+    for j in str2:
+        count += 1
+        if j in dictionary2:
+            dictionary2[j] += 1
+        else:
+            dictionary2[j] = 1
+    if dictionary1 == dictionary2: answer = True
+    toc = time.perf_counter()
+    print(dictionary1)
+    print(dictionary2)
+    print(count)
+    print(f"{toc - tic:.8f}")
+    return answer
 
 
 str1 = "the alias men"
 str2 = "alan smithee"
 
 print(alineaA(str1, str2))
+print("--------------------------------------------------------------")
 print(alineaB(str1, str2))
+print("--------------------------------------------------------------")
+print(alineaC(str1, str2))
+print("--------------------------------------------------------------")
+print(alineaD(str1, str2))
+
+
+print("Conclusão -> com base nos testes executados com diferentes strings, conseguimos concluir que o algoritmo com menos passos é o da alínea b) enquanto que o algoritmo mais rápido é o da alínea d) (com uma diferença na ordem dos ~10^-6s)")
