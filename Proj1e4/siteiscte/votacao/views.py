616637da090d770e14f5e6c438facdf210699e1d
carlotasousa <Carlota_Sousa@iscte-iul.p>
2023-03-15 00:17:34 +0000

    done semana 4

--- Proj1e4/siteiscte/votacao/views.py
+++ Proj1e4/siteiscte/votacao/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Questao
 
 def index(request):
-    return HttpResponse("Pagina de entrada da app votacao.")
-# Create your views here.
+    latest_question_list = Questao.objects.order_by('-pub_data')[:5]
+    output = ', '.join([q.questao_texto for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def detalhe(request, questao_id):
+    return HttpResponse("Esta e a questao %s." %questao_id)
+def resultados(request, questao_id):
+    response = "Estes sao os resultados da questao %s."
+    return HttpResponse(response % questao_id)
+def voto(request, questao_id):
+    return HttpResponse("Votacao na questao %s." %questao_id)
